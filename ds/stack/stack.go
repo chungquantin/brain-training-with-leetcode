859f6bf4f5b0639b7b79c332e2ef8b83413e7ee5
Tin Chung <cqtin0903@gmail.com>
2022-02-09 20:19:01 +0700

    fix stack and queue

--- ds/stack/stack.go
+++ ds/stack/stack.go
@@ -10,20 +10,15 @@ type Stack struct {
 	maxLength int32
 	s         []interface{}
 	pos       int32
-	order     int32
 }
 
-// FILO: 0 - LIFO: 1
-func NewStack(maxLength_ int32, order_ int32) *Stack {
-	if (order_ != 0) && (order_ != 1) {
-		panic("ERROR: Invalid stack order")
-	}
+// LIFO: 1
+func NewStack(maxLength_ int32) *Stack {
 	return &Stack{
 		m:         new(sync.Mutex),
 		maxLength: maxLength_,
 		s:         make([]interface{}, 0),
 		pos:       0,
-		order:     order_,
 	}
 }
 
@@ -36,13 +31,9 @@ func (stack *Stack) Push(v interface{}) {
 	if v == nil {
 		panic("ERROR: Can't push nil into stack")
 	}
-	if stack.order == 0 {
-		stack.s = append(stack.s, v)
-	} else {
-		tempStack := make([]interface{}, 0)
-		tempStack = append(tempStack, v)
-		stack.s = append(tempStack, stack.s...)
-	}
+	tempStack := make([]interface{}, 0)
+	tempStack = append(tempStack, v)
+	stack.s = append(tempStack, stack.s...)
 	stack.pos++
 }
 
@@ -57,13 +48,9 @@ func (stack *Stack) Pop() interface{} {
 	}
 
 	var rv interface{}
-	if stack.order == 0 {
-		rv = stack.s[stack.pos-1]
-		stack.s = stack.s[:stack.pos-1]
-	} else {
-		rv = stack.s[0]
-		stack.s = stack.s[1:]
-	}
+
+	rv = stack.s[0]
+	stack.s = stack.s[1:]
 	stack.pos--
 	return rv
 }
@@ -84,8 +71,8 @@ func (stack *Stack) isEmpty() bool {
 }
 
 func main() {
-	fmt.Println("FILO Stack")
-	stack := NewStack(10, 0)
+	fmt.Println("LIFO Stack")
+	stack := NewStack(10)
 	stack.Push(1)
 	fmt.Println(stack.s...)
 	stack.Push("Hi")
@@ -101,22 +88,4 @@ func main() {
 
 	topValue := stack.Peek()
 	fmt.Println("Top value: ", topValue)
-
-	fmt.Println("LIFO Stack")
-	stack1 := NewStack(10, 1)
-	stack1.Push(1)
-	fmt.Println(stack1.s...)
-	stack1.Push("Hi")
-	fmt.Println(stack1.s...)
-	stack1.Push(2)
-	fmt.Println(stack1.s...)
-	stack1.Push(5)
-	fmt.Println(stack1.s...)
-
-	poppedValue1 := stack1.Pop()
-	fmt.Println("Popped value: ", poppedValue1)
-	fmt.Println(stack1.s...)
-
-	topValue1 := stack.Peek()
-	fmt.Println("Top value: ", topValue1)
 }
