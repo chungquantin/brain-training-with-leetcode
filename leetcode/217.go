509c605f4e94c37f51dff49015108d3d1f1d207b
Tin Chung <cqtin0903@gmail.com>
2022-02-10 08:53:41 +0700

    leetcode (217) rust and go

---
+++ leetcode/217.go
@@ -0,0 +1,16 @@
+package leetcode
+
+// 217. Contains Duplicates
+
+// TIME: O(n)
+// LeetCode: Runtime: 91ms - Memory: 10.1MB
+func containsDuplicate(nums []int) bool {
+	numDuplicateCheck := make(map[int]bool, 0)
+	for _, num := range nums {
+		if numDuplicateCheck[num] == true {
+			return true
+		}
+		numDuplicateCheck[num] = true
+	}
+	return false
+}
